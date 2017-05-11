250f8ec917e1a8005e0995578fefabdab8804bb2
Johan DEQUEANT <jdequeant@gmail.com>
2017-05-11 16:51:42 +0200

    Update motd

--- motd.tcl
+++ motd.tcl
@@ -3,7 +3,7 @@ let secs=$((${upSeconds}%60))
 let mins=$((${upSeconds}/60%60))
 let hours=$((${upSeconds}/3600%24))
 let days=$((${upSeconds}/86400))
-UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+UPTIME=`printf "%d days, %02d:%02d:%02d" "$days" "$hours" "$mins" "$secs"`
 
 memavailable=`cat /proc/meminfo | grep MemAvailable | awk '{printf("%d", $2/1024)}'`
 memtotal=`cat /proc/meminfo | grep MemTotal | awk '{printf("%d", $2/1024)}'`
@@ -29,7 +29,7 @@ echo "${BOLD}${GREEN}
    .~ .~~~..~.    
   : .~.'~'.~. :   ${YELLOW}Uptime.............: ${REGULAR}${CYAN}${UPTIME}${RED}${BOLD}
  ~ (   ) (   ) ~  ${YELLOW}Memory.............: ${REGULAR}${CYAN}${memallocated}/${memtotal}MB${RED}${BOLD}
-( : '~'.~.'~' : ) ${YELLOW}Load Averages......: ${REGULAR}${CYAN}${one}, ${five}, ${fifteen} (1, 5, 15 min)${RED}${BOLD}
+( : '~'.~.'~' : ) ${YELLOW}Load Averages......: ${REGULAR}${CYAN}${one} ${five} ${fifteen} (1 5 15 min)${RED}${BOLD}
  ~ .~ (   ) ~. ~  ${YELLOW}Running Processes..: ${REGULAR}${CYAN}`ps ax | wc -l | tr -d " "`${RED}${BOLD}
   (  : '~' :  )   ${YELLOW}Local IP...........: ${REGULAR}${CYAN}`/sbin/ifconfig eth0 | /bin/grep "inet addr" | /usr/bin/cut -d ":" -f 2 | /usr/bin/cut -d " " -f 1`${RED}${BOLD}
    '~ .~~~. ~'    ${YELLOW}Public IP .........: ${REGULAR}${CYAN}`wget -q -O - http://icanhazip.com/ | tail`${RED}${BOLD}
